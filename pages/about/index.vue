82a8db47f2e23862ba1a5869603c470fc96a9f7d
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2022-02-02 01:06:15 +0900

    started blog posts page

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <h1>thsi is my fantatic about page!!</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page{
+  text-align: center;
+  padding: 30px;
+}
+</style>
