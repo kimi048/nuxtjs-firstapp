d28a5e899adc9526c58c0cd490cc90c28b1ec9a4
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2022-02-03 20:01:54 +0900

    created post page

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      thumbnail="/web.jpg"
+      title="Hello there!"
+      previewText="This is my first post!!"
+    />
+    <PostPreview
+      id="2"
+      thumbnail="/web.jpg"
+      title="Hello there - the second post!"
+      previewText="This is my second post!!"
+    />
+    <PostPreview
+      id="3"
+      thumbnail="/web.jpg"
+      title="Hello there- the third post!"
+      previewText="This is my third post!!"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default{
+  components:{
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,35 +4,17 @@
       <h1>Get the latest tech news!</h1>
     </section>
 
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        thumbnail="/web.jpg"
-        title="Hello there!"
-        previewText="This is my first post!!"
-      />
-      <PostPreview
-        id="2"
-        thumbnail="/web.jpg"
-        title="Hello there - the second post!"
-        previewText="This is my second post!!"
-      />
-      <PostPreview
-        id="3"
-        thumbnail="/web.jpg"
-        title="Hello there- the third post!"
-        previewText="This is my third post!!"
-      />
-    </section>
+    <PostList/>
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview'
+import PostList from '@/components/Posts/PostList';
 export default {
   components: {
-    PostPreview,
-  },
+    
+    PostList
+},
 }
 </script>
 
